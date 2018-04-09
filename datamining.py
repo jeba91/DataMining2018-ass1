507109a3d52106bd9ef07d3e1d594855f4aa602e
Jeroen Baars <jeroen.j.baars@gmail.com>
2018-04-09 19:24:17 +0200

    comment and picture

--- datamining.py
+++ datamining.py
@@ -19,21 +19,30 @@ dataset.set_index(keys=['variable'], drop=False,inplace=True)
 # get a list of names
 variables = dataset['variable'].unique().tolist()
 # now we can perform a lookup on a 'view' of the dataframe
+
+
 varias = []
 
+#Loop through all variable names from the dataset and perform interpolation
+#Variable names are for example [mood, sms, call ...]
 for x in variables:
+    #append the dataset with only values for one variable
     varias.append(dataset.loc[dataset.variable==x])
 
-# Count how much missing values we have for each variable;
-for y in range(0, len(varias)):
-    print(variables[y])
-    print(len(varias[y]))
-    print(varias[y].isnull().sum())
+#loop through the list of variables values
+#Count how much missing values we have for each variable;
+#THIS ONE ONLY NEEDED FOR MISSING VALUE SEARCH
+#for y in range(0, len(varias)):
+    #print variable name
+    #print(variables[y])
+    #print amount of values
+    #print(len(varias[y]))
+    #print amount of NA values in variable value dataset
+    #print(varias[y].isnull().sum())
 
 
 # Missing Values
 class ImputationMissingValues:
-
     # Interpolate the dataset based on previous/next values..
     def impute_interpolate(self, dataset, col):
         dataset[col] = dataset[col].interpolate()
@@ -48,44 +57,46 @@ imputed_interpolation_dataset = MisVal.impute_interpolate(copy.deepcopy(dataset)
 # DataViz.plot_imputed_values(dataset, ['original', 'interpolation'], 'value', dataset['value'], imputed_interpolation_dataset['value'])
 #print imputed_interpolation_dataset
 
-def mood_per_participant():
+def mood_per_participant(variable_name):
     dataset.sort_values(by=['id'])
     # set the index to be this and don't drop
     dataset.set_index(keys=['id'], drop=False, inplace=True)
-    # get a list of names
+    # get a list of id names
+    # Total nummer of id's = 27 (some are missing!)
     id_person = dataset['id'].unique().tolist()
-    print(id_person)
+
+    #Get empty list for ids
     ids = []
 
-
-
-    print(dataset.id)
-
-
+    #Loop through al unique IDs and get Values
+    #Then get values for certain ID and variable name
     for x in id_person:
+        #get only for one id
         data = dataset.loc[dataset.id == x]
-        ids.append(data.loc[data.variable == 'mood'])
-
-    #for y in ids:
-    print(ids[0])
-    print(ids[0]['time'].values)
-    print(ids[0]['value'].values)
+        #and the one value
+        ids.append(data.loc[data.variable == variable_name])
 
     fig, axes = plt.subplots(6,5,sharex = True, sharey=True )
 
+    #for subplots
     a = 0
     b = 0
 
-    print(len(ids))
-
+    #create a subplot of the value against time for every ID
     for y in range(0,len(ids)):
-        print(a,b)
+        #print(a,b)
         axes[a,b].plot(ids[y]['time'].values,ids[y]['value'].values)
         a = a+1
         if a == 6:
             b = b+1
             a = 0
 
-    plt.show()
+    #save the plot
+    savename = 'pictures/' + variable_name + '.png'
+    fig.savefig(savename)
+    #plt.show()
 
-mood_per_participant()
+#create images for every variable, saved in pictures
+for name in variables:
+    print(name)
+    mood_per_participant(name)
