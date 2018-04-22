f3eb9529ccd7fb9b80fa89186bc14a70a52684cd
Jeroen Baars <jeroen.j.baars@gmail.com>
2018-04-22 19:44:36 +0200

    hop

--- 3_ARIMA.py
+++ 3_ARIMA.py
@@ -82,28 +82,33 @@ for id in id_person:
 
 
 
-# from sklearn.metrics import mean_squared_error
-#
-# X = data_id['mood'].values
-# size = int(len(X) * 0.66)
-# train, test = X[0:size], X[size:len(X)]
-# history = [x for x in train]
-# predictions = list()
-# for t in range(len(test)):
-# 	model = ARIMA(history, order=(1,1,0))
-# 	model_fit = model.fit(disp=0)
-# 	output = model_fit.forecast()
-# 	yhat = output[0]
-# 	predictions.append(yhat)
-# 	obs = test[t]
-# 	history.append(obs)
-# 	print('predicted=%f, expected=%f' % (yhat, obs))
-# error = mean_squared_error(test, predictions)
-# print('Test MSE: %.3f' % error)
-# # plot
-# pyplot.plot(test)
-# pyplot.plot(predictions, color='red')
-# pyplot.savefig('ding')
+from sklearn.metrics import mean_squared_error
+
+for id in id_person:
+	fig = plt.figure()
+	train_id = train_set.loc[train_set['id'] == id]
+	test_id =  test_set.loc[train_set['id'] == id]
+	train = train_id['mood'].values.tolist()
+	X = 
+	size = int(len(X) * 0.66)
+	train, test = X[0:size], X[size:len(X)]
+	history = [x for x in train]
+	predictions = list()
+	for t in range(len(test)):
+		model = ARIMA(history, order=(1,1,0))
+		model_fit = model.fit(disp=0)
+		output = model_fit.forecast()
+		yhat = output[0]
+		predictions.append(yhat)
+		obs = test[t]
+		history.append(obs)
+		print('predicted=%f, expected=%f' % (yhat, obs))
+	error = mean_squared_error(test, predictions)
+	print('Test MSE: %.3f' % error)
+	# plot
+	pyplot.plot(test)
+	pyplot.plot(predictions, color='red')
+	pyplot.savefig('arima/' + id + '.png')
 
 # X = data_id['mood']
 # Y = range(len(data_id['mood']))
