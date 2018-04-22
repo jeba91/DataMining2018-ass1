c3bd898dfe079de1fedd72d725a705d672ca4e08
Jeroen Baars <jeroen.j.baars@gmail.com>
2018-04-22 18:25:44 +0200

    ACF

--- 3_ARIMA.py
+++ 3_ARIMA.py
@@ -29,35 +29,85 @@ from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 from sklearn.feature_selection import RFECV
 from sklearn.ensemble import RandomForestClassifier
+from statsmodels.tsa.stattools import acf, pacf
 
 import pandas
 import pandas as pd
 
 #Load in filtered data
-data_all = pd.read_pickle('filtered_data_train.pkl')
-
-#Create new variable of call/sms combined
-data_all['call/sms'] = data_all['call'] + data_all['sms']
-data_all.pop('call')
-data_all.pop('sms')
+data_train = pd.read_pickle('filtered_data_train.pkl')
+data_test = pd.read_pickle('filtered_data_test.pkl')
 
 #Add ordinal dates
-data_all['ordinal'] = data_all.index.values
+data_train['ordinal'] = data_train.index.values
+data_test['ordinal'] = data_test.index.values
 
 # get a list of unique ID's
-id_person = data_all['id'].unique().tolist()
+id_person = data_train['id'].unique().tolist()
 
-# sort the dataframe by variables
-data_all.sort_values(by=['id'])
-# set the index to be this and don't drop
-data_all.set_index(keys=['id'], drop=False, inplace=True)
+# # sort the dataframe by variables
+# data_train.sort_values(by=['id'])
+# data_train.set_index(keys=['id'], drop=False, inplace=True)
+# data_test.sort_values(by=['id'])
+# data_test.set_index(keys=['id'], drop=False, inplace=True)
 
-train_set = data_all[['id','ordinal','mood']]
-data_id = train_set.loc[train_set['id'] == 'AS14.33']
+train_set = data_train[['id','ordinal','mood']]
+test_set = data_test[['id','ordinal','mood']]
 
-X = data_id['mood']
-Y = range(len(data_id['mood']))
+print(train_set)
 
+for id in id_person:
+	fig = plt.figure()
+	train_id = train_set.loc[train_set['id'] == id]
+	test_id =  test_set.loc[train_set['id'] == id]
+
+	lag_acf = acf((np.log(train_id['mood'])), nlags=20)
+	lag_pacf = pacf((np.log(train_id['mood'])), nlags=20, method='ols')
+
+	plt.subplot(121)
+	plt.plot(lag_acf)
+	plt.axhline(y=0,linestyle='--',color='gray')
+	plt.axhline(y=-1.96/np.sqrt(len(np.log(train_id['mood']))),linestyle='--',color='gray')
+	plt.axhline(y=1.96/np.sqrt(len(np.log(train_id['mood']))),linestyle='--',color='gray')
+	plt.title('Autocorrelation Function')
+
+	plt.subplot(122)
+	plt.plot(lag_pacf)
+	plt.axhline(y=0,linestyle='--',color='gray')
+	plt.axhline(y=-1.96/np.sqrt(len(np.log(train_id['mood']))),linestyle='--',color='gray')
+	plt.axhline(y=1.96/np.sqrt(len(np.log(train_id['mood']))),linestyle='--',color='gray')
+	plt.title('Partial Autocorrelation Function')
+	plt.tight_layout()
+	plt.savefig('acfpacf/' + id + '.png')
+
+
+
+# from sklearn.metrics import mean_squared_error
+#
+# X = data_id['mood'].values
+# size = int(len(X) * 0.66)
+# train, test = X[0:size], X[size:len(X)]
+# history = [x for x in train]
+# predictions = list()
+# for t in range(len(test)):
+# 	model = ARIMA(history, order=(1,1,0))
+# 	model_fit = model.fit(disp=0)
+# 	output = model_fit.forecast()
+# 	yhat = output[0]
+# 	predictions.append(yhat)
+# 	obs = test[t]
+# 	history.append(obs)
+# 	print('predicted=%f, expected=%f' % (yhat, obs))
+# error = mean_squared_error(test, predictions)
+# print('Test MSE: %.3f' % error)
+# # plot
+# pyplot.plot(test)
+# pyplot.plot(predictions, color='red')
+# pyplot.savefig('ding')
+
+# X = data_id['mood']
+# Y = range(len(data_id['mood']))
+#
 # plt.plot(Y, X)
 # plt.ylabel('Mood')
 # plt.show()
@@ -68,26 +118,6 @@ Y = range(len(data_id['mood']))
 # autocorrelation_plot(np.log(data_id['mood']))
 # pyplot.show()
 
-from statsmodels.tsa.stattools import acf, pacf
-
-lag_acf = acf((np.log(data_id['mood'])), nlags=20)
-lag_pacf = pacf((np.log(data_id['mood'])), nlags=20, method='ols')
-
-plt.subplot(121)
-plt.plot(lag_acf)
-plt.axhline(y=0,linestyle='--',color='gray')
-plt.axhline(y=-1.96/np.sqrt(len(np.log(data_id['mood']))),linestyle='--',color='gray')
-plt.axhline(y=1.96/np.sqrt(len(np.log(data_id['mood']))),linestyle='--',color='gray')
-plt.title('Autocorrelation Function')
-
-plt.subplot(122)
-plt.plot(lag_pacf)
-plt.axhline(y=0,linestyle='--',color='gray')
-plt.axhline(y=-1.96/np.sqrt(len(np.log(data_id['mood']))),linestyle='--',color='gray')
-plt.axhline(y=1.96/np.sqrt(len(np.log(data_id['mood']))),linestyle='--',color='gray')
-plt.title('Partial Autocorrelation Function')
-plt.tight_layout()
-plt.show()
 #
 # # fit model
 # model = ARIMA(np.log(data_id['mood']), order=(7, 1, 0))
@@ -103,27 +133,3 @@ plt.show()
 # print(residuals.describe())
 # pyplot.show()
 # print(data_id)
-
-
-from sklearn.metrics import mean_squared_error
-
-X = data_id['mood'].values
-size = int(len(X) * 0.66)
-train, test = X[0:size], X[size:len(X)]
-history = [x for x in train]
-predictions = list()
-for t in range(len(test)):
-	model = ARIMA(history, order=(1,1,0))
-	model_fit = model.fit(disp=0)
-	output = model_fit.forecast()
-	yhat = output[0]
-	predictions.append(yhat)
-	obs = test[t]
-	history.append(obs)
-	print('predicted=%f, expected=%f' % (yhat, obs))
-error = mean_squared_error(test, predictions)
-print('Test MSE: %.3f' % error)
-# plot
-pyplot.plot(test)
-pyplot.plot(predictions, color='red')
-pyplot.savefig('ding')
